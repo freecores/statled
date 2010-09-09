938e58b7ca401b1363555cad08f3d0cb6602d0b0
mitko <mitko@c415cf9c-f37d-4a9e-97e2-16f6774b9eea>
2010-09-09 06:35:32 +0000

    standard header added

--- rtl/statled.v
+++ rtl/statled.v
@@ -1,11 +1,41 @@
+//////////////////////////////////////////////////////////////////////
+////  statled.v                                                   ////   
+////                                                              ////
+////  This file is part of the Status LED module.                 ////
+////  http://www.opencores.org/projects/statled/                  ////
+////                                                              ////
+////  Author:                                                     ////
+////     -Dimitar Dimitrov, d.dimitrov@bitlocker.eu               ////
+////                                                              ////
+//////////////////////////////////////////////////////////////////////
+////                                                              ////
+//// Copyright (C) 2010 Authors                                   ////
+////                                                              ////
+//// This source file may be used and distributed without         ////
+//// restriction provided that this copyright statement is not    ////
+//// removed from the file and that any derivative work contains  ////
+//// the original copyright notice and the associated disclaimer. ////
+////                                                              ////
+//// This source file is free software; you can redistribute it   ////
+//// and/or modify it under the terms of the GNU Lesser General   ////
+//// Public License as published by the Free Software Foundation; ////
+//// either version 2.1 of the License, or (at your option) any   ////
+//// later version.                                               ////
+////                                                              ////
+//// This source is distributed in the hope that it will be       ////
+//// useful, but WITHOUT ANY WARRANTY; without even the implied   ////
+//// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      ////
+//// PURPOSE.  See the GNU Lesser General Public License for more ////
+//// details.                                                     ////
+////                                                              ////
+//// You should have received a copy of the GNU Lesser General    ////
+//// Public License along with this source; if not, download it   ////
+//// from http://www.opencores.org/lgpl.shtml                     ////
+////                                                              ////
+//////////////////////////////////////////////////////////////////////
+
 `timescale 1ns / 100ps
-/******************************************************************************
-*  Status LED module
-*
-*  Use single LED ouput to displays various internal states as blink codes.
-*  http://www.opencores.org/cores/statled
-*
-******************************************************************************/
+
 module statled (
     input           clk,
     input           rst,
@@ -22,11 +52,11 @@ reg [15:0]	cr;             // Code register
 reg [3:0] 	str;            // Status register
 wire 		rate;           // LED rate
 
-//-----------------------------------------------------------------------------
+//--------------------------------------------------------------------
 // LED rate  
 //
 always @(posedge clk or posedge rst)
-	if (rst) 
+    if (rst) 
         pre <= #tDLY 0;
     else if (rate)
         pre <= #tDLY 0;
@@ -35,7 +65,7 @@ always @(posedge clk or posedge rst)
 
 assign rate = (pre == STATLED_PULSE_CLKCNT);
 
-//-----------------------------------------------------------------------------
+//--------------------------------------------------------------------
 // Capture status inputs
 //
 always @(posedge clk or posedge rst)
@@ -44,7 +74,7 @@ always @(posedge clk or posedge rst)
     else 
         str <= #tDLY status;
 
-//-----------------------------------------------------------------------------
+//--------------------------------------------------------------------
 // Shift register and bit counter
 //
 always @(posedge clk or posedge rst)
@@ -65,7 +95,7 @@ always @(posedge clk or posedge rst)
 
 assign led = rst? 1 : lsr[15];	
 
-//-----------------------------------------------------------------------------
+//--------------------------------------------------------------------
 // Codes 
 //
 always @*
